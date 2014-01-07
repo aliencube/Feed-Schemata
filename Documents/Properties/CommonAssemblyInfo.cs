9b101c2897439b43125bcb12ddf0b3e199676ed5
Justin Yoo <justin.yoo@aliencube.com>
2014-01-07 22:42:21 +1100

    Relocated RDF Site Summary Schema project.

--- Documents/Properties/CommonAssemblyInfo.cs
+++ Documents/Properties/CommonAssemblyInfo.cs
@@ -5,22 +5,10 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SiteSummarySchema.1.0")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SiteSummarySchema.1.0")]
+[assembly: AssemblyCompany("Aliencube Community")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyTrademark("Aliencube")]
 [assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("a2847e5d-0aa8-4390-ae16-971860e0ea76")]
 
 // Version information for an assembly consists of the following four values:
 //
--- SourceCodes/RDF/SiteSummarySchema.1.0/Properties/AssemblyInfo.cs
+++ SourceCodes/RDF/SiteSummarySchema.1.0/Properties/AssemblyInfo.cs
@@ -5,10 +5,10 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Aliencube.FeedSchemata.Rss.Rss20")]
+[assembly: AssemblyTitle("Aliencube.FeedSchemata.Rdf.SiteSummary10")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyProduct("Aliencube.FeedSchemata.Rss.Rss20")]
+[assembly: AssemblyProduct("Aliencube.FeedSchemata.Rdf.SiteSummary10")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -16,4 +16,4 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("7ec32c2e-2f64-4bed-bfc9-94b8f7e4ba3a")]
+[assembly: Guid("a2847e5d-0aa8-4390-ae16-971860e0ea76")]
