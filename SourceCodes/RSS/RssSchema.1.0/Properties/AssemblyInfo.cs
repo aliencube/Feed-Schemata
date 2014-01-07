de5b9e353db8dc50b4f66cd3bb02693511eed497
Justin Yoo <justin.yoo@aliencube.com>
2014-01-08 00:06:35 +1100

    Relocated RSS 1.0 schema.

--- SourceCodes/RSS/RssSchema.1.0/Properties/AssemblyInfo.cs
+++ SourceCodes/RSS/RssSchema.1.0/Properties/AssemblyInfo.cs
@@ -5,10 +5,10 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Aliencube.FeedSchemata.Rdf.SiteSummary10")]
+[assembly: AssemblyTitle("Aliencube.FeedSchemata.Rss.Rss10")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyProduct("Aliencube.FeedSchemata.Rdf.SiteSummary10")]
+[assembly: AssemblyProduct("Aliencube.FeedSchemata.Rss.Rss10")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
